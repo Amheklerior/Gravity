a85d07626133667cae9f3d4c2751408b3c78e487
Andrea <andrea.amato.89@gmail.com>
2020-02-29 08:47:06 +0000

    Implements input handling and spaceship movement

---
+++ project/Assets/Player/code/input/InputControls.cs
@@ -0,0 +1,262 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Player/code/input/InputControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+namespace Amheklerior.Gravity.Player
+{
+    public class @InputControls : IInputActionCollection, IDisposable
+    {
+        public InputActionAsset asset { get; }
+        public @InputControls()
+        {
+            asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputControls"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""e56f9115-4173-4fd8-aabb-1271441c5846"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""9616f706-376a-4e66-989c-d8087c38bfc8"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Rotate"",
+                    ""type"": ""Value"",
+                    ""id"": ""bbb17a46-2b45-4316-aa0c-05ecab227d14"",
+                    ""expectedControlType"": ""Axis"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""f000166f-2856-4f2c-ab63-49fcf401e685"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""95af1d43-72dc-4eb7-b09a-028c4a3f0b6c"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""3356336c-9e04-45b9-979d-648d2a8bc4d3"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""26590aa7-8d8a-4e66-b34d-8198b41f7bbb"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Rotate"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""a19ebaf4-f872-4a5f-987b-89fd5bb1cee9"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Rotate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""50bd98c8-09d6-4e1e-a119-ca10a37428b9"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Rotate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Arrows"",
+                    ""id"": ""69679293-769d-4edb-956d-5ec16087f995"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Rotate"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""Negative"",
+                    ""id"": ""2b684322-3c99-41f3-b85f-3dee4aef3b85"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Rotate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Positive"",
+                    ""id"": ""637baf7b-46c9-4cfc-8e92-cd3733498a02"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Rotate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard"",
+            ""bindingGroup"": ""Keyboard"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+            // Gameplay
+            m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+            m_Gameplay_Move = m_Gameplay.FindAction("Move", throwIfNotFound: true);
+            m_Gameplay_Rotate = m_Gameplay.FindAction("Rotate", throwIfNotFound: true);
+        }
+
+        public void Dispose()
+        {
+            UnityEngine.Object.Destroy(asset);
+        }
+
+        public InputBinding? bindingMask
+        {
+            get => asset.bindingMask;
+            set => asset.bindingMask = value;
+        }
+
+        public ReadOnlyArray<InputDevice>? devices
+        {
+            get => asset.devices;
+            set => asset.devices = value;
+        }
+
+        public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+        public bool Contains(InputAction action)
+        {
+            return asset.Contains(action);
+        }
+
+        public IEnumerator<InputAction> GetEnumerator()
+        {
+            return asset.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Enable()
+        {
+            asset.Enable();
+        }
+
+        public void Disable()
+        {
+            asset.Disable();
+        }
+
+        // Gameplay
+        private readonly InputActionMap m_Gameplay;
+        private IGameplayActions m_GameplayActionsCallbackInterface;
+        private readonly InputAction m_Gameplay_Move;
+        private readonly InputAction m_Gameplay_Rotate;
+        public struct GameplayActions
+        {
+            private @InputControls m_Wrapper;
+            public GameplayActions(@InputControls wrapper) { m_Wrapper = wrapper; }
+            public InputAction @Move => m_Wrapper.m_Gameplay_Move;
+            public InputAction @Rotate => m_Wrapper.m_Gameplay_Rotate;
+            public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+            public void Enable() { Get().Enable(); }
+            public void Disable() { Get().Disable(); }
+            public bool enabled => Get().enabled;
+            public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+            public void SetCallbacks(IGameplayActions instance)
+            {
+                if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+                {
+                    @Move.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                    @Move.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                    @Move.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                    @Rotate.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnRotate;
+                    @Rotate.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnRotate;
+                    @Rotate.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnRotate;
+                }
+                m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+                if (instance != null)
+                {
+                    @Move.started += instance.OnMove;
+                    @Move.performed += instance.OnMove;
+                    @Move.canceled += instance.OnMove;
+                    @Rotate.started += instance.OnRotate;
+                    @Rotate.performed += instance.OnRotate;
+                    @Rotate.canceled += instance.OnRotate;
+                }
+            }
+        }
+        public GameplayActions @Gameplay => new GameplayActions(this);
+        private int m_KeyboardSchemeIndex = -1;
+        public InputControlScheme KeyboardScheme
+        {
+            get
+            {
+                if (m_KeyboardSchemeIndex == -1) m_KeyboardSchemeIndex = asset.FindControlSchemeIndex("Keyboard");
+                return asset.controlSchemes[m_KeyboardSchemeIndex];
+            }
+        }
+        public interface IGameplayActions
+        {
+            void OnMove(InputAction.CallbackContext context);
+            void OnRotate(InputAction.CallbackContext context);
+        }
+    }
+}
---
+++ project/Assets/Player/code/movement/SpaceshipController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Amheklerior.Gravity.Player {
+
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class SpaceshipController : MonoBehaviour {
+
+        [SerializeField] [Range(10f, 50f)] private float movementSpeed = 25f;
+        [SerializeField] [Range(1f, 5f)] private float rotationalSpeed = 1f;
+
+        private InputControls _inputControls;
+        private Rigidbody2D _rigidbody;
+        private Transform _transform;
+        private bool _isMoving;
+        private float _rotationalDirection;
+
+        #region Setup
+
+        private void Awake() {
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _transform = transform;
+            
+            _inputControls = new InputControls();
+            _inputControls.Gameplay.Move.performed += ctx => _isMoving = true;
+            _inputControls.Gameplay.Move.canceled += ctx => _isMoving = false;
+            _inputControls.Gameplay.Rotate.performed += ctx => _rotationalDirection = -ctx.ReadValue<float>();
+            _inputControls.Gameplay.Rotate.canceled += ctx => _rotationalDirection = 0f;
+        }
+
+        private void OnEnable() => _inputControls.Gameplay.Enable();
+        private void OnDisable() => _inputControls.Gameplay.Disable();
+
+        #endregion
+
+        private void FixedUpdate() {
+            if (_isMoving) Move();
+            if (_rotationalDirection != 0f) Rotate();
+        }
+        
+        private void Move() => _rigidbody.AddForce(transform.up * movementSpeed * Time.deltaTime, ForceMode2D.Impulse);
+
+        private void Rotate() => _rigidbody.AddTorque(_rotationalDirection * rotationalSpeed * Time.deltaTime, ForceMode2D.Impulse);
+        
+    }
+}
