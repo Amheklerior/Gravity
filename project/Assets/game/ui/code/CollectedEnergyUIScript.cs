dfc1c2d40b4401796df99a72563cc9281553ce1f
Andrea <andrea.amato.89@gmail.com>
2020-03-08 14:57:31 +0000

    UI elements binding

---
+++ project/Assets/game/ui/code/CollectedEnergyUIScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Amheklerior.Gravity.Util;
+
+namespace Amheklerior.Gravity.UI {
+
+    public class CollectedEnergyUIScript : MonoBehaviour {
+
+        [SerializeField] private IntVariable currentlyCollected;
+        [SerializeField] private IntVariable requiredToCollect;
+        [SerializeField] private Text element;
+
+        private void Update() {
+            element.text = currentlyCollected.CurrentValue + " / " + requiredToCollect.CurrentValue;
+        }
+
+    }
+}
---
+++ project/Assets/game/ui/code/EngineHeatUIScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Amheklerior.Gravity.Util;
+
+namespace Amheklerior.Gravity.UI {
+
+    public class EngineHeatUIScript : MonoBehaviour {
+
+        [SerializeField] private SpriteRenderer heatFillSprite;
+        [SerializeField] private FloatVariable currentEngineHeat;
+
+        private float _maxFillValue = 425f;
+        private float _engineMaxHeat = 30f;
+        private float Ratio => _maxFillValue / _engineMaxHeat;
+
+        private void Update() {
+            heatFillSprite.size = new Vector2(currentEngineHeat.CurrentValue * Ratio, heatFillSprite.size.y);
+        }
+        
+    }
+}
